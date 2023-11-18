9478c24e4c86cf6e013c944aca21801ff3bab4f9
Flo <flo@uwap.org>
2023-11-18 20:38:38 +0100

    Added a worker that deletes accounts that have requested their deletion at least 30 days ago.

---
+++ Worker.cs
@@ -0,0 +1,15 @@
+﻿namespace uwap.WebFramework.Plugins;
+
+public partial class UsersPlugin : Plugin
+{
+    public override Task Work()
+    {
+        foreach (var table in Server.Config.Accounts.UserTables.Values.Distinct())
+            foreach (var kv in table)
+                if (kv.Value.Settings.TryGetValue("Delete", out var ticksString)
+                    && long.TryParse(ticksString, out var ticks)
+                    && (new DateTime(ticks) + TimeSpan.FromDays(30)) < DateTime.UtcNow)
+                    table.Delete(kv.Key);
+        return Task.CompletedTask;
+    }
+}
