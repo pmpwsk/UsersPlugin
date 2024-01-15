36a75e558fa13c27bedb7d6bd20e1af91e278426
Flo <flo@uwap.org>
2024-01-15 23:24:54 +0100

    Changed theme.js for the new theme structure.

--- Files/settings/theme.js
+++ Files/settings/theme.js
@@ -1,6 +1,14 @@
-async function Set(name) {
-    let response = await fetch("/api[PATH_PREFIX]/settings/theme?name=" + name);
-    if (response.status === 200) {
-        window.location.reload();
+async function Save() {
+    try {
+        let response = await fetch("/api[PATH_PREFIX]/settings/theme"
+            + "?f=" + document.querySelector("#font").value
+            + "&b=" + document.querySelector("#background").value
+            + "&a=" + document.querySelector("#accent").value
+            + "&d=" + document.querySelector("#design").value);
+        if (response.status === 200)
+            window.location.reload();
+        else ShowError("Connection failed.");
+    } catch {
+        ShowError("Connection failed.");
     }
 }
