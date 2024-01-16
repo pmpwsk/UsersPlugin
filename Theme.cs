8ee4fdc44ec3eeaf67ba60ab31d35a500028c787
Flo <flo@uwap.org>
2024-01-16 02:39:32 +0100

    Added hash sets for possible theme components.

---
+++ Theme.cs
@@ -0,0 +1,12 @@
+﻿using Org.BouncyCastle.Asn1.Ocsp;
+using uwap.WebFramework.Elements;
+
+namespace uwap.WebFramework.Plugins;
+
+public partial class UsersPlugin : Plugin
+{
+    private readonly HashSet<string> Fonts = ["ubuntu", "ubuntu-mono"];
+    private readonly HashSet<string> Backgrounds = ["black", "dark", "light", "white"];
+    private readonly HashSet<string> Accents = ["green", "violet", "blue", "red"];
+    private readonly HashSet<string> Designs = ["shadows", "layers", "flat", "no-css"];
+}
