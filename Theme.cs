c92d68bd5008c0d325d4de824a980416555e4131
Flo <flo@uwap.org>
2024-01-16 02:41:39 +0100

    Added Timestamp(...).

--- Theme.cs
+++ Theme.cs
@@ -15,6 +15,16 @@ public partial class UsersPlugin : Plugin
     public string DefaultAccent = "blue";
     public string DefaultDesign = "layers";
 
+    private string Timestamp(string font, string? fontMono, string background, string accent, string design)
+        => ((string[])
+        [
+            $"/theme/f/{font}.css",
+            ..(fontMono == null ? (string[])[] : ([$"/theme/f/{fontMono}.css"])),
+            $"/theme/b/{background}.css",
+            $"/theme/a/{AccentName(accent, background)}.css",
+            $"/theme/d/{design}.css"
+        ]).Max(x => long.Parse(GetFileVersion(x) ?? "0")).ToString();
+
     private static string AccentName(string accent, string background)
         => $"{accent}-{background switch
         {
