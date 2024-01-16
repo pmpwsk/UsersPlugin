851cb7b7a51a1f1f1fc4987e854d8160212f0163
Flo <flo@uwap.org>
2024-01-16 02:39:57 +0100

    Added default values for the theme components.

--- Theme.cs
+++ Theme.cs
@@ -9,4 +9,9 @@ public partial class UsersPlugin : Plugin
     private readonly HashSet<string> Backgrounds = ["black", "dark", "light", "white"];
     private readonly HashSet<string> Accents = ["green", "violet", "blue", "red"];
     private readonly HashSet<string> Designs = ["shadows", "layers", "flat", "no-css"];
+
+    public string DefaultFont = "ubuntu";
+    public string DefaultBackground = "dark";
+    public string DefaultAccent = "blue";
+    public string DefaultDesign = "layers";
 }
