8b754a890eaf10f6fea098a38d6335cdc44563be
Flo <flo@uwap.org>
2024-01-17 21:51:11 -0200

    Added "oxygen" to the possible fonts.

--- Theme.cs
+++ Theme.cs
@@ -5,7 +5,7 @@ namespace uwap.WebFramework.Plugins;
 
 public partial class UsersPlugin : Plugin
 {
-    private readonly HashSet<string> Fonts = ["ubuntu", "ubuntu-mono"];
+    private readonly HashSet<string> Fonts = ["ubuntu", "ubuntu-mono", "oxygen"];
     private readonly HashSet<string> Backgrounds = ["black", "dark", "light", "white"];
     private readonly HashSet<string> Accents = ["green", "violet", "blue", "red"];
     private readonly HashSet<string> Designs = ["shadows", "layers", "flat", "no-css"];
