a4ffab1abc6d40c18a6f2ff02957f8503dd0faa7
pmpwsk <flo@uwap.org>
2026-03-15 23:55:12 +0100

    Removed apps.js.

--- Files/settings/apps.js
+++
@@ -1,5 +0,0 @@
-async function Remove(index, name, expires) {
-    if ((await fetch(`apps/remove?index=${index}&name=${name}&expires=${expires}`, {method:"POST"})).status === 200)
-        window.location.reload();
-    else ShowError("Connection failed.");
-}
